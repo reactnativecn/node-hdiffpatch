eedead3fc6a6eb131c63950a0f5828393f97c8aa
sisong <sisong@gmail.com>
2021-04-07 11:25:41 +0800

    test hdiff ok

--- src/hdiff.h
+++ src/hdiff.h
@@ -7,7 +7,7 @@
 #include <stdint.h>
 #include <vector>
 
-voif hdiff(const uint8_t* old,size_t oldsize,const uint8_t* _new,size_t newsize,
+void hdiff(const uint8_t* old,size_t oldsize,const uint8_t* _new,size_t newsize,
 		   std::vector<uint8_t>& out_codeBuf);
 
 #endif
